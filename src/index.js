6ef8f059d68b257e7bddfe8aa22f4a917acf6312
chiui <chhiuili@gmail.com>
2024-05-29 22:55:36 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,28 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+const proxyPath = '/https://'
+
+export default {
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url)
+    const pathname = url.pathname
+    if (pathname.startsWith(proxyPath)) {
+      const targetUrl = pathname.slice(1)
+      const headers = new Headers(request.headers)
+      headers.set('host', url.host)
+      return fetch(targetUrl, {
+        method: request.method,
+        headers: headers,
+      })
+    }
+    return new Response('Hello World!');
+  },
+};
